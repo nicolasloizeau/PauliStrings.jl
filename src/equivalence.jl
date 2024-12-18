12ce4cff71594b697ff175b2dd67fb3554baaaaa
nicolasloizeau <nicolasloizeau@gmail.com>
2024-12-18 16:16:58 +0100

    add equivalence.jl

---
+++ src/equivalence.jl
@@ -0,0 +1,27 @@
+
+"""
+    equivalence_class(A1::Operator, H::Operator)
+
+Equivalence class of A1 with respect to Hamiltonian H, as defined in [https://arxiv.org/abs/2406.13026](https://arxiv.org/abs/2406.13026) Algorithm 1.
+The equivalence class is returned as an operator with coeficients 1
+Use [`get_pauli`](@ref) to access individual strings.
+"""
+
+function equivalence_class(A1::Operator, H::Operator)
+    A = deepcopy(A1)
+    D = 1
+    counter = 0
+    while counter < D
+        Vv = A.v[counter+1]
+        Vw = A.w[counter+1]
+        for i in 1:length(H)
+            a, Av, Aw = com(Vv, Vw, H.v[i], H.w[i])
+            if a != 0 && !vw_in_o(Av, Aw, A)
+                push!(A, 1, Av, Aw)
+                D += 1
+            end
+        end
+        counter += 1
+    end
+    return A
+end
