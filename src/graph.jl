7b1e553b76f5d4eb564b6e5bf72aa8a2bd356c73
Nicolas Loizeau <nicolas.loizeau@nbi.ku.dk>
2024-12-31 00:00:31 +0100

    update frustration_graph

--- src/graph.jl
+++ src/graph.jl
@@ -1,24 +1,22 @@
 
-using Graphs
-
-
-
+using SparseArrays
 """
     frustration_graph(o::Operator)
 
-Construct the frustration graph, or anticommutation graph of an operator o. Returns a SimpleGraph from Graphs.jl.
+Construct the frustration graph, or anticommutation graph of an operator o. Returns a sparse adjacency matrix.
 Each vertex represents a pauli string of the operator, and two vertices are connected by an edge if the corresponding pauli strings anticommute.
 The vertices are labeled by the index of the pauli string in the operator. Use [`get_pauli`](@ref) to access individual strings.
 """
 function frustration_graph(o::Operator)
-    G = SimpleGraph(length(o))
-    for i in 1:length(o)
-        for j in 1:length(o)
+    n = length(o)
+    G = spzeros(Int, n, n)
+    for i in 1:n
+        for j in 1:n
             v = o.v[i] ⊻ o.v[j]
             w = o.w[i] ⊻ o.w[j]
             k = (-1)^count_ones(o.v[i] & o.w[j]) - (-1)^count_ones(o.w[i] & o.v[j])
             if k != 0
-                add_edge!(G, i, j)
+                G[i, j] = 1
             end
         end
     end
