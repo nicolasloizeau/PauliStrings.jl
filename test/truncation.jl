1eb7146a23025b6323fa575ec3b982aa29c99e38
Juan Bernate <juanebernate@gmail.com>
2025-05-28 07:24:04 -0500

    Add more tests, fix latex in index

--- test/truncation.jl
+++ test/truncation.jl
@@ -1,3 +1,23 @@
+"""
+Creates a random 2-local operator with qubit q only supporting the identity.
+"""
+function rand_local2_qubit_id(N::Int, q::Int)
+    o = Operator(N)
+    for i in 1:N
+        for j in 1:N
+            if i != j
+                for k in ['X', 'Y', 'Z']
+                    for l in ['X', 'Y', 'Z']
+                        k = (i == q) ? 'I' : k
+                        l = (j == q) ? 'I' : l
+                        o += (randn(rng, Float64), k, i, l, j)
+                    end
+                end
+            end
+        end
+    end
+    return compress(o)
+end
 
 @testset "truncation" begin
     N = 8
@@ -14,6 +34,10 @@
     @test opnorm(ps.add_noise(O2, g)) < opnorm(O2)
     @test opnorm(ps.add_noise(O2, g)) < opnorm(ps.add_noise(O2, 0.2 * g))
     @test opnorm(Operator(ps.add_noise(O2, fill(0.5, N)) - ps.add_noise(O2, 0.5))) < 1e-10
+    q = rand(1:N) # qubit only supporting identity
+    g = [i == q ? rand() : 0.0 for i in 1:N] # noise only affecting qubit q
+    O2q = rand_local2_qubit_id(N, q)
+    @test opnorm(Operator(ps.add_noise(O2q, g) - O2q)) < 1e-10
     @test opnorm(k_local_part(O2, 1) - ps.truncate(O2, 1)) == 0
     O1 = ising1D(N, 0.5)
     O1ts = OperatorTS1D(O1)
