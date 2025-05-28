b0c72710a0c1036e8bb5638020ee068c0ab98e24
Juan Bernate <juanebernate@gmail.com>
2025-05-28 07:51:35 -0500

    Export add_dephasing_noise, add test

--- test/truncation.jl
+++ test/truncation.jl
@@ -1,3 +1,23 @@
+"""
+ Creates a random 2-local operator with qubit q only supporting 'I' or 'Z'.
+"""
+function rand_local2_qubit_diag(N::Int, q::Int)
+    o = Operator(N)
+    for i in 1:N
+        for j in 1:N
+            if i != j
+                for k in ['X', 'Y', 'Z']
+                    k = (i == q && k != 'Z') ? rand(['I', 'Z']) : k
+                    for l in ['X', 'Y', 'Z']
+                        l = (j == q && l != 'Z') ? rand(['I', 'Z']) : l
+                        o += (randn(rng, Float64), k, i, l, j)
+                    end
+                end
+            end
+        end
+    end
+    return compress(o)
+end
 
 @testset "truncation" begin
     N = 8
@@ -12,9 +32,13 @@
     @test opnorm(ps.add_noise(O2, 0.5)) < opnorm(ps.add_noise(O2, 0.1))
     g = rand(N)
     @test opnorm(ps.add_dephasing_noise(O2, g)) < opnorm(O2)
-    @test opnorm(ps.add_dephasing_noise(O2, g)) < opnorm(ps.add_dephasing_noise(O2, 0.2*g))
+    @test opnorm(ps.add_dephasing_noise(O2, g)) < opnorm(ps.add_dephasing_noise(O2, 0.2 * g))
     @test opnorm(Operator(ps.add_dephasing_noise(O2, fill(0.5, N)) - ps.add_dephasing_noise(O2, 0.5))) < 1e-10
     @test opnorm(Operator(ps.diag(ps.add_dephasing_noise(O2, g)) - ps.diag(O2))) < 1e-10
+    q = rand(1:N) # qubit only supporting 'I' or 'Z'
+    g = [i == q ? rand() : 0.0 for i in 1:N] # noise only affecting qubit q
+    O2q = rand_local2_qubit_diag(N, q)
+    @test opnorm(Operator(ps.add_dephasing_noise(O2q, g) - O2q)) < 1e-10
     @test opnorm(k_local_part(O2, 1) - ps.truncate(O2, 1)) == 0
     O1 = ising1D(N, 0.5)
     O1ts = OperatorTS1D(O1)
