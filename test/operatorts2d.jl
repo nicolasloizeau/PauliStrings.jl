26df1fd53ec5519b5fd38cd4770997aee665ad3a
nicolasloizeau <nicolasloizeau@gmail.com>
2025-08-12 16:23:02 +0200

    ts2d tests

--- test/operatorts2d.jl
+++ test/operatorts2d.jl
@@ -7,6 +7,8 @@
         O1ts = OperatorTS2D(O1, L1)
         O2ts = OperatorTS2D(O2, L1)
         eps = 1e-8
+        @test (opnorm(O1) - opnorm(O1ts)) < eps
+        @test (opnorm(O2) - opnorm(O2ts)) < eps
         @test typeof(O1ts + O2ts) == typeof(O1ts)
         @test typeof(O1ts - O2ts) == typeof(O1ts)
         @test typeof(O1ts * O2ts) == typeof(O1ts)
@@ -21,13 +23,13 @@
         @test opnorm(Operator(O1ts + O2ts) - (O1 + O2)) < eps
         @test opnorm(Operator(O1ts * 7) - O1 * 7) < eps
         @test opnorm(Operator(O1ts + 7) - (O1 + 7)) < eps
-        @test is_ts2d(O1,L1)
-        @test is_ts2d(O1*O2,L1)
-        @test !is_ts2d(ps.rand_local2(L1*L2),L1)
+        @test is_ts2d(O1, L1)
+        @test is_ts2d(O1 * O2, L1)
+        @test !is_ts2d(ps.rand_local2(L1 * L2), L1)
         @test trace(O1ts) == trace(dagger(O1))
         @test trace(O1ts) == trace(diag(O1ts))
         k = 1 + L1
-        @test isapprox(trace_product(O1ts, k),trace_product(O1, k))
+        @test isapprox(trace_product(O1ts, k), trace_product(O1, k))
         @test isapprox(trace_product(O1ts, k), trace(O1^k))
         @test isapprox(trace_product(O1ts, k), trace(O1ts^k))
     end
